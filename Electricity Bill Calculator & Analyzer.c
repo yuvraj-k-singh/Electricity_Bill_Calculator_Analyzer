a6b7e31bfcf62893cd2ffae6a90ee115b0e08b17
yuvraj-k-singh <yuvrajstudentbussiness@gmail.com>
2026-01-01 19:29:12 +0530

    first commit

---
+++ Electricity Bill Calculator & Analyzer.c
@@ -0,0 +1,28 @@
+/*
+Project: Advanced Calculator Using C programming
+Date: 24-aug-2025
+Developer: Yuvraj Kumar Singh
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int choice, user_index = -1;
+	
+	while(1){
+		printf("\n=================================================\n");
+    	printf("      Electricity Bill Calculator & Analyzer\n");
+    	printf("=================================================\n");
+    	printf("1. Add New Consumer\n");
+    	printf("2. Calculate Bill\n");
+    	printf("3. Store Monthly Bill\n");
+    	printf("4. Power Saving Tips\n");
+    	printf("5. Exit\n");
+    	printf("--------------------------------------------------\n");
+    	printf("Enter your choice: ");
+    	scanf("%d", &choice);
+	}
+	return 0;
+}
+
